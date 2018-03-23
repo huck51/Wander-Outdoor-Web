cb0f9bbaf98c52142a9e8e375d2216bb21ffa6eb
Huck <evanallen51@gmail.com>
2018-03-23 18:27:55 -0500

    Updated styles

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <NavigationBar />
         <p>Where will you wander?
         </p>
-        <SearchBar />
+        <SearchBar id="LPSearch" />
 
       </div>
     );
