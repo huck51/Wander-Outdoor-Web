6cb7f3987694e6d91654654a8f0eb1368de1dfc1
Huck <evanallen51@gmail.com>
2018-03-22 23:35:24 -0500

    Adding searchbar component

---
+++ src/Components/searchbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './Images/WanderLogo.png';
+import './App.css';
+
+class searchbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default searchbar;
