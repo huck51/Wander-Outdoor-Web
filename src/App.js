cc14bc79ef6879848d7035600a93759b7907281e
Huck <evanallen51@gmail.com>
2018-04-18 03:31:32 -0500

    Added footer to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import NavigationBar from './Components/navigationbar';
 import Main from './main';
+import Footer from './Components/footer';
 import './App.css';
 
 const App = () => (
   <div>
     <NavigationBar />
     <Main />
+    </Footer>
   </div>
 );
 
--- src/Components/footer.js
+++ src/Components/footer.js
@@ -7,6 +7,8 @@ class Footer extends Component {
   render() {
     return (
       <div>
+        <a href="https://www.facebook.com/wanderoutdoor.co/" target="_blank">Facebook</a>
+        <a href="https://www.instagram.com/wanderoutdoorllc/" target="_blank">Instagram</a>
       </div>
     );
   }
