7aa101d0f6ae28e357b830a82e65e92bdcd4cb06
Huck <evanallen51@gmail.com>
2018-04-18 03:38:51 -0500

    Added a little bit of style to the footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -6,9 +6,13 @@ class Footer extends Component {
 
   render() {
     return (
-      <div>
-        <a href="https://www.facebook.com/wanderoutdoor.co/" target="_blank">Facebook</a>
-        <a href="https://www.instagram.com/wanderoutdoorllc/" target="_blank">Instagram</a>
+      <div className="footer">
+        <div>
+          <a href="https://www.facebook.com/wanderoutdoor.co/" target="_blank">Facebook</a>
+        </div>
+        <div>
+          <a href="https://www.instagram.com/wanderoutdoorllc/" target="_blank">Instagram</a>
+        </div>
       </div>
     );
   }
