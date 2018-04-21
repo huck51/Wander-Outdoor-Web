d95fd66905fcbd820c0e09576ca3febf3a78fed2
Huck <evanallen51@gmail.com>
2018-04-20 20:11:49 -0500

    Linted about

--- src/Containers/about.js
+++ src/Containers/about.js
@@ -1,19 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Styles/about.css';
 
 
-class About extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>ABOUT US!</h1>
-      </div>
-    );
-  }
-}
+const About = () => (
+  <div>
+    <h1>ABOUT US!</h1>
+  </div>
+);
 
 export default About;
