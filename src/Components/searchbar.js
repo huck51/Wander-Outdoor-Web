5301ea3d88b6fd744d97bc406385581aaecfdbef
Huck <evanallen51@gmail.com>
2018-04-20 20:27:27 -0500

    Linted searchbar

--- src/Components/searchbar.js
+++ src/Components/searchbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './Styles/searchbar.css';
 
 class SearchBar extends Component {
@@ -8,12 +9,17 @@ class SearchBar extends Component {
     super(props);
     this.state = {
       value: '',
-    }
+    };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
-    this.setState({value: e.target.value});
+    this.setState({ value: e.target.value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    axios.get('https://fierce-ridge-55021.herokuapp.com/results');
   }
 
   render() {
@@ -21,14 +27,14 @@ class SearchBar extends Component {
       <div>
         <form>
           <input
-          type="text"
-          placeholder="Search..."
-          value={this.state.value}
-          onChange={this.handleChange}>
-          </input>
-          <Link to='/results'>
+            type="text"
+            placeholder="Search..."
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <Link to="/results">
             <Button type="submit">
-              <i className="fas fa-search"></i>
+              <i className="fas fa-search" />
             </Button>
           </Link>
         </form>
