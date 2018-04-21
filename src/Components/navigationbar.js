9c368a2f8cee28220c55a517b4b85f6bdd78a438
Huck <evanallen51@gmail.com>
2018-04-20 20:31:50 -0500

    Linted navigationbar

--- src/Components/navigationbar.js
+++ src/Components/navigationbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Navbar,
   Nav,
@@ -11,51 +11,46 @@ import { Link } from 'react-router-dom';
 import Logo from '../Images/WanderLogoWide.png';
 import './Styles/navigationbar.css';
 
-class NavigationBar extends Component {
-
-  render() {
-    return (
-      <Navbar collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#brand"><Image id="navlogo" src={Logo} responsive /></a>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              <Link to="/">Home</Link>
-            </NavItem>
-            <NavItem eventKey={1} href="#">
-              <Link to="/guiding-companies">Guiding Companies</Link>
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              <Link to="/guides">Guides</Link>
-            </NavItem>
-            <NavItem eventKey={1} href="#">
-              <Link to="/about">About Us</Link>
-            </NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
-            </NavDropdown>
-          </Nav>
-          <Nav pullRight>
-            <NavItem eventKey={1} href="#">
-              <Link to="/signup">Sign Up</Link>
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              <Link to="/login">Login</Link>
-            </NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
-}
+const NavigationBar = () => (
+  <Navbar collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#brand"><Image id="navlogo" src={Logo} responsive /></a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#">
+          <Link to="/">Home</Link>
+        </NavItem>
+        <NavItem eventKey={1} href="#">
+          <Link to="/guiding-companies">Guiding Companies</Link>
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+          <Link to="/guides">Guides</Link>
+        </NavItem>
+        <NavItem eventKey={1} href="#">
+          <Link to="/about">About Us</Link>
+        </NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">
+          <Link to="/signup">Sign Up</Link>
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+          <Link to="/login">Login</Link>
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
 export default NavigationBar;
