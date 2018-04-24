7c68d9f20b193379bd38911457bb02cef3a13e0a
Huck <evanallen51@gmail.com>
2018-04-24 16:51:35 -0500

    Created redux store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import * as reducers from './Reducers/TBD'; // Change this when file created
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
 
 ReactDOM.render(
   (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   ), document.getElementById('root'),
 );
 registerServiceWorker();
