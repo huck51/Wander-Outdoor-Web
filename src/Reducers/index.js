b7467d465e77296b55e0caeff1bf680ec8baebb6
Huck <evanallen51@gmail.com>
2018-04-24 16:28:27 -0500

    Built out rootReducer
    TODO:
    -Change imported file on line 2

---
+++ src/Reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import * as reducers from './TBD'; // Change this to file that I create to hold combineReducers
+
+const rootReducer = combineReducers(reducers);
+
+export default rootReducer;
