e83765c21ccb9eddce0010b51ec506203bd22431
Huck <evanallen51@gmail.com>
2018-04-25 00:29:54 -0500

    Updated and reformatted signUpGC change methods

--- src/Containers/signUpGC.jsx
+++ src/Containers/signUpGC.jsx
@@ -17,47 +17,11 @@ class SignUpGC extends Component {
       password: '',
       verifyPW: '',
     };
-
-    this.handleCompanyChange = this.handleCompanyChange.bind(this);
-    this.handleAddressChange = this.handleAddressChange.bind(this);
-    this.handleCompanyPhoneChange = this.handleCompanyPhoneChange.bind(this);
-    this.handleContactNameChange = this.handleContactNameChange.bind(this);
-    this.handleJobTitleChange = this.handleJobTitleChange.bind(this);
-    this.handleContactPhoneChange = this.handleContactPhoneChange.bind(this);
-    this.handleContactEmailChange = this.handleContactEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleVerifyPWChange = this.handleVerifyPWChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  handleCompanyChange(e) {
-    this.setState({ companyName: e.target.value });
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   }
-  handleAddressChange(e) {
-    this.setState({ companyAddress: e.target.value });
-  }
-  handleCompanyPhoneChange(e) {
-    this.setState({ companyPhone: e.target.value });
-  }
-  handleContactNameChange(e) {
-    this.setState({ contactName: e.target.value });
-  }
-  handleJobTitleChange(e) {
-    this.setState({ jobTitle: e.target.value });
-  }
-  handleContactPhoneChange(e) {
-    this.setState({ contactPhone: e.target.value });
-  }
-  handleContactEmailChange(e) {
-    this.setState({ contactEmail: e.target.value });
-  }
-  handlePasswordChange(e) {
-    this.setState({ password: e.target.value });
-  }
-  handleVerifyPWChange(e) {
-    this.setState({ verifyPW: e.target.value });
-  }
-  handleSubmit(e) {
+  handleSubmit = (e) => {
     e.preventDefault();
     const {
       companyName,
@@ -110,63 +74,63 @@ class SignUpGC extends Component {
             type="text"
             placeholder="Company Name"
             value={this.state.companyName}
-            onChange={this.handleCompanyChange}
+            onChange={this.handleChange}
           />
           <input
             name="companyAddress"
             type="text"
             placeholder="Company Address"
             value={this.state.companyAddress}
-            onChange={this.handleAddressChange}
+            onChange={this.handleChange}
           />
           <input
             name="companyPhone"
             type="text"
             placeholder="Company Phone"
             value={this.state.companyPhone}
-            onChange={this.handleCompanyPhoneChange}
+            onChange={this.handleChange}
           />
           <input
             name="contactName"
             type="text"
             placeholder="Contact Name"
             value={this.state.contactName}
-            onChange={this.handleContactNameChange}
+            onChange={this.handleChange}
           />
           <input
             name="jobTitle"
             type="text"
             placeholder="Job Title"
             value={this.state.jobTitle}
-            onChange={this.handleJobTitleChange}
+            onChange={this.handleChange}
           />
           <input
             name="contactPhone"
             type="text"
             placeholder="Contact Phone"
             value={this.state.contactPhone}
-            onChange={this.handleContactPhoneChange}
+            onChange={this.handleChange}
           />
           <input
             name="contactEmail"
             type="email"
             placeholder="E-mail"
             value={this.state.contactEmail}
-            onChange={this.handleContactEmailChange}
+            onChange={this.handleChange}
           />
           <input
             name="password"
             type="password"
             placeholder="Password"
             value={this.state.password}
-            onChange={this.handlePasswordChange}
+            onChange={this.handleChange}
           />
           <input
             name="verifyPW"
             type="password"
             placeholder="Verify Password"
             value={this.state.verifyPW}
-            onChange={this.handleVerifyPWChange}
+            onChange={this.handleChange}
           />
           <button type="submit" onClick={this.handleSubmit}>Submit</button>
         </form>
