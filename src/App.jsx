df6ba4d0e4ba6820e9ab58f7df97c36b2787d132
Huck <evanallen51@gmail.com>
2018-05-03 01:04:15 -0500

    Implemented random background pictures on landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,29 @@ import React from 'react';
 import NavigationBar from './Components/navigationbar';
 import Main from './main';
 import Footer from './Components/footer';
+import bouldering from './Images/BackgroundImages/bouldering.jpg';
+import canyonRun from './Images/BackgroundImages/canyonRun.jpg';
+import deepSnowHike from './Images/BackgroundImages/deepSnowHike.jpg';
+import grayTightRope from './Images/BackgroundImages/grayTightRope.jpg';
+import hangingGear from './Images/BackgroundImages/hangingGear.jpg';
+import sunsetClimb from './Images/BackgroundImages/sunsetClimb.jpg';
+import surfing from './Images/BackgroundImages/surfing.jpg';
 import './App.css';
 
+const backgroundArray = [bouldering, canyonRun, deepSnowHike, grayTightRope,
+  hangingGear, sunsetClimb, surfing];
+const rando = Math.floor(Math.random() * backgroundArray.length);
+const stylz = {
+  backgroundImage: `url(${backgroundArray[rando]})`,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  height: '100vh',
+};
+
 const App = () => (
-  <div>
+  <div style={stylz}>
+    {console.log(window.location)}
     <div id="body">
       <NavigationBar />
       <Main />
--- src/Containers/home.jsx
+++ src/Containers/home.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import bouldering from '../Images/BackgroundImages/bouldering.jpg';
 import SearchBar from '../Components/searchbar';
+import './Styles/home.css';
 import '../App.css';
 
 class Home extends Component {
   render() {
     return (
-      <div style={{backgroundImage: `url(${bouldering})`}} className="sizing">
-        <h2 id="topQ">Where will you wander?</h2>
-        <SearchBar id="LPSearch" />
-        <h2 id="bottomQ">What will you do?</h2>
+      <div>
+        <div>
+          <h2 id="topQ">Where will you wander?</h2>
+          <SearchBar id="LPSearch" />
+          <h2 id="bottomQ">What will you do?</h2>
+        </div>
       </div>
     );
   }
