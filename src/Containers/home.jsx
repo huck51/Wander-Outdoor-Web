104ac7e1ca1ae46ef82703428e7e5e762188c27a
Huck <evanallen51@gmail.com>
2018-05-02 23:47:52 -0500

    Added stylesheet for home

--- src/Containers/home.jsx
+++ src/Containers/home.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import bouldering from '../Images/BackgroundImages/bouldering.jpg';
 import SearchBar from '../Components/searchbar';
 import '../App.css';
 
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div style={{backgroundImage: `url(${bouldering})`}} className="sizing">
         <h2 id="topQ">Where will you wander?</h2>
         <SearchBar id="LPSearch" />
         <h2 id="bottomQ">What will you do?</h2>
