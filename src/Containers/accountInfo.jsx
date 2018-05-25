e6756d72f230879c070e08bc1a2e23491c3ec0b2
Huck <evanallen51@gmail.com>
2018-05-25 14:15:50 -0500

    Built out more of accountInfo page; Added delete account button
    TODO:
    -Finish handleSubmit function
    -Finish handleDelete function
    -Finish componentDidMount function

--- src/Containers/accountInfo.jsx
+++ src/Containers/accountInfo.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FormGroup, HelpBlock, Radio } from 'react-bootstrap';
 import FieldGroup from '../Components/fieldGroup';
 import './Styles/accountInfo.css';
 
@@ -7,8 +8,25 @@ class AccountInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: {},
+      test1: '',
+      test2: '',
+      test3: '',
+      roleGroup: '',
+      explorer: true,
+      guide: false,
     };
+  }
+
+  componentDidMount() {
+
+  }
+
+  handleSubmit = (e) => {
+
+  }
+
+  handleDelete = () => {
+    
   }
 
   render() {
@@ -17,6 +35,23 @@ class AccountInfo extends Component {
         <h1>ACCOUNT INFO</h1>
         <div className="container">
           <form>
+            <FormGroup>
+              <Radio
+                onClick={this.handleClick}
+                value="guide"
+                name="roleGroup"
+                inline
+                checked={this.state.guide}
+                >Guide</Radio>
+              <Radio
+                onClick={this.handleClick}
+                value="explorer"
+                name="roleGroup"
+                inline
+                checked={this.state.explorer}
+                >Expolorer</Radio>
+              <HelpBlock>*Please only select "Guide" if you are a certified guide currently employed by a company</HelpBlock>
+            </FormGroup>
             <FieldGroup
               type="text"
               label="Test 1"
@@ -32,6 +67,16 @@ class AccountInfo extends Component {
               label="Test 3"
               placeholder="Placeholder 3"
             />
+            <button
+              className="epSaveBtn"
+              type="submit"
+              onClick={this.handleSubmit}
+            >Save
+            </button>
+            <FormGroup>
+              <button className="btn-danger deleteBtn" onClick={this.handleDelete}>Delete My Account</button>
+              <HelpBlock>*Deleting your acccount is a permanent action and cannot be undone!!!</HelpBlock>
+            </FormGroup>
           </form>
         </div>
       </div>
