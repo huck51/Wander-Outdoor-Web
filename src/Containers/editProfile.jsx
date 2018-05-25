84e7deafe87a1ce6454dbe2502c3a759422c7215
Huck <evanallen51@gmail.com>
2018-05-25 13:06:38 -0500

    Implemented state, onClick function, and onChange function
    TODO:
    -Finish onSubmit function

--- src/Containers/editProfile.jsx
+++ src/Containers/editProfile.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FormGroup, HelpBlock, Radio } from 'react-bootstrap';
+import axios from 'axios';
 import FieldGroup from '../Components/fieldGroup';
 import './Styles/editProfile.css';
 
@@ -8,12 +9,30 @@ class EditProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: {},
+        test1: '',
+        test2: '',
+        test3: '',
+        roleGroup: 'explorer',
+        explorer: true,
+        guide: false,
     };
   }
 
-  handleSubmit = () => {
+  handleClick = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+      explorer: !this.state.explorer,
+      guide: !this.state.guide,
+    });
+  }
 
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.put('https://fierce-ridge-55021.herokuapp.com/update-profile');
   }
 
   render() {
@@ -23,27 +42,48 @@ class EditProfile extends Component {
         <div className="container">
           <form onSubmit={this.handleSubmit}>
             <FormGroup>
-              <Radio inline>Guide</Radio>
-              <Radio inline>Expolorer</Radio>
+              <Radio
+                onClick={this.handleClick}
+                value="guide"
+                name="roleGroup"
+                inline
+                checked={this.state.guide}
+                >Guide</Radio>
+              <Radio
+                onClick={this.handleClick}
+                value="explorer"
+                name="roleGroup"
+                inline
+                checked={this.state.explorer}
+                >Expolorer</Radio>
               <HelpBlock>*Please only select "Guide" if you are a certified guide currently employed by a company</HelpBlock>
             </FormGroup>
             <FieldGroup
+              name="test1"
               type="text"
               label="Test 1"
               placeholder="Placeholder 1"
+              onChange={this.handleChange}
+              value={this.state.test1}
             />
             <FieldGroup
+              name="test2"
               type="text"
               label="Test 2"
               placeholder="Placeholder 2"
+              onChange={this.handleChange}
+              value={this.state.test2}
             />
             <FieldGroup
+              name="test3"
               type="text"
               label="Test 3"
               placeholder="Placeholder 3"
+              onChange={this.handleChange}
+              value={this.state.test3}
             />
           <button
-            className="epSaveBtn" 
+            className="epSaveBtn"
             type="submit"
             onClick={this.handleSubmit}
             >Save</button>
