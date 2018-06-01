095f0a9daa63b77fc6ea14e9254c2da7e2b3b641
Huck <evanallen51@gmail.com>
2018-06-01 05:33:36 -0500

    Added image uploader and previewer forcompany registration

--- src/Containers/signUpGC.jsx
+++ src/Containers/signUpGC.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Col, Row, } from 'react-bootstrap';
 import axios from 'axios';
 import FieldGroup from '../Components/fieldGroup';
 import './Styles/signUpGC.css';
 
+const previewConfig = [null];
 
 class SignUpGC extends Component {
   constructor(props) {
@@ -15,8 +17,8 @@ class SignUpGC extends Component {
       jobTitle: '',
       contactPhone: '',
       contactEmail: '',
-      password: '',
-      verifyPW: '',
+      imageFile: null,
+      picture: '',
     };
   }
   handleChange = (e) => {
@@ -32,7 +34,7 @@ class SignUpGC extends Component {
       jobTitle,
       contactPhone,
       contactEmail,
-      password,
+      imageFile
     } = this.state;
     const newCompany = {
       companyName,
@@ -42,7 +44,7 @@ class SignUpGC extends Component {
       jobTitle,
       contactPhone,
       contactEmail,
-      password,
+      imageFile
     };
     this.setState({
       companyName: '',
@@ -52,8 +54,8 @@ class SignUpGC extends Component {
       jobTitle: '',
       contactPhone: '',
       contactEmail: '',
-      password: '',
-      verifyPW: '',
+      imageFile: null,
+      picture: ''
     });
     axios.post('https://fierce-ridge-55021.herokuapp.com/signup/guiding-company', newCompany)
       .then(() => {
@@ -65,74 +67,113 @@ class SignUpGC extends Component {
       });
   }
 
+  fileChangeHandler = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    console.log(file);
+    reader.onloadend = () => {
+      this.setState({
+        imageFile: file,
+        picture: reader.result
+      });
+      const configAmmo = {
+        backgroundImage: `url(${this.state.picture})`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '30em',
+        width: '100%'
+      }
+      previewConfig[0] = configAmmo;
+    }
+    reader.readAsDataURL(file);
+  }
+
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Guiding Company SignUp</h1>
         <div className="container">
-          <form onSubmit={this.handleSubmit} className="sizeControl">
-            <FieldGroup
-              label="Company Name"
-              name="companyName"
-              type="text"
-              placeholder="Company Name"
-              value={this.state.companyName}
-              onChange={this.handleChange}
-            />
-            <FieldGroup
-              label="Company Address"
-              name="companyAddress"
-              type="text"
-              placeholder="Company Address"
-              value={this.state.companyAddress}
-              onChange={this.handleChange}
-            />
-            <FieldGroup
-              label="Company Phone Number"
-              name="companyPhone"
-              type="text"
-              placeholder="Company Phone"
-              value={this.state.companyPhone}
-              onChange={this.handleChange}
-            />
-            <FieldGroup
-              label="Contact Name"
-              name="contactName"
-              type="text"
-              placeholder="Contact Name"
-              value={this.state.contactName}
-              onChange={this.handleChange}
-            />
-            <FieldGroup
-              label="Job Title"
-              name="jobTitle"
-              type="text"
-              placeholder="Job Title"
-              value={this.state.jobTitle}
-              onChange={this.handleChange}
-            />
-            <FieldGroup
-              label="Contact Phone Number"
-              name="contactPhone"
-              type="text"
-              placeholder="Contact Phone"
-              value={this.state.contactPhone}
-              onChange={this.handleChange}
-            />
-            <FieldGroup
-              label="Contact Email"
-              name="contactEmail"
-              type="email"
-              placeholder="E-mail"
-              value={this.state.contactEmail}
-              onChange={this.handleChange}
-            />
-            <button
-              type="submit"
-              onClick={this.handleSubmit}
-              className="epSaveBtn"
-            >Submit</button>
-          </form>
+          <Row>
+            <Col xs={12} sm={12} md={6} lg={8}>
+              <form onSubmit={this.handleSubmit} className="sizeControl">
+                <FieldGroup
+                  label="Company Name"
+                  name="companyName"
+                  type="text"
+                  placeholder="Company Name"
+                  value={this.state.companyName}
+                  onChange={this.handleChange}
+                />
+                <FieldGroup
+                  label="Company Address"
+                  name="companyAddress"
+                  type="text"
+                  placeholder="Company Address"
+                  value={this.state.companyAddress}
+                  onChange={this.handleChange}
+                />
+                <FieldGroup
+                  label="Company Phone Number"
+                  name="companyPhone"
+                  type="text"
+                  placeholder="Company Phone"
+                  value={this.state.companyPhone}
+                  onChange={this.handleChange}
+                />
+                <FieldGroup
+                  label="Contact Name"
+                  name="contactName"
+                  type="text"
+                  placeholder="Contact Name"
+                  value={this.state.contactName}
+                  onChange={this.handleChange}
+                />
+                <FieldGroup
+                  label="Job Title"
+                  name="jobTitle"
+                  type="text"
+                  placeholder="Job Title"
+                  value={this.state.jobTitle}
+                  onChange={this.handleChange}
+                />
+                <FieldGroup
+                  label="Contact Phone Number"
+                  name="contactPhone"
+                  type="text"
+                  placeholder="Contact Phone"
+                  value={this.state.contactPhone}
+                  onChange={this.handleChange}
+                />
+                <FieldGroup
+                  label="Contact Email"
+                  name="contactEmail"
+                  type="email"
+                  placeholder="E-mail"
+                  value={this.state.contactEmail}
+                  onChange={this.handleChange}
+                />
+                <FieldGroup
+                  label="Logo/Company Photo"
+                  type="file"
+                  onChange={this.fileChangeHandler}
+                  name="picture"
+                />
+                <button
+                  type="submit"
+                  onClick={this.handleSubmit}
+                  className="epSaveBtn"
+                >Submit</button>
+              </form>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={4}>
+              <div className="container">
+                <h4>Preview Logo/Company Photo</h4>
+                  <img src={this.state.picture} className="profPic"/>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
     );
