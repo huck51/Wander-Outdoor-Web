7a3bf4d6bfba04d94f68f44ae3ddbf368343880f
Huck <evanallen51@gmail.com>
2018-06-05 00:37:01 -0500

    Changed Description field in addTrip to a textarea and implemented an upload widget

--- src/Containers/addTrip.jsx
+++ src/Containers/addTrip.jsx
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import {
+  Col,
+  ControlLabel,
+  FormControl,
+  FormGroup,
+  Row,
+} from 'react-bootstrap';
 import axios from 'axios';
 import FieldGroup from '../Components/fieldGroup';
 import './Styles/addTrip.css';
@@ -12,6 +19,7 @@ class AddTrip extends Component {
       description: '',
       price: '',
       location: '',
+      picture: '',
     };
   }
   handleChange = (e) => {
@@ -48,50 +56,100 @@ class AddTrip extends Component {
       });
   }
 
+  uploadWidget = () => {
+    const cloudData = {
+      paramsToSign: {
+        eager: 'w_400,h_300,c_pad|w_260,h_200,c_crop',
+        public_id: 'userImage',
+      }
+    }
+    axios.post('https://fierce-ridge-55021.herokuapp.com/cloudinary', cloudData)
+      .then((response) => {
+        console.log(response.data);
+        const widgetOptions = {
+          cloud_name: 'wander-outdoor',
+          signature: response.data,
+          upload_preset: 'ypvspamw'
+        };
+        window.cloudinary.openUploadWidget(widgetOptions, (err, result) => {
+          if (err) {
+            console.log(err);
+          }
+          if (result) {
+            console.log(result);
+            this.setState({
+              picture: result[0].secure_url,
+            });
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>Add Trip</h1>
         <div className="container">
-          <form onSubmit={this.handleSubmit} className="sizeControl">
-            <FieldGroup
-              label="Trip Name"
-              name="name"
-              type="text"
-              placeholder="Name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <FieldGroup
-                label="Trip location"
-                name="location"
-                type="text"
-                placeholder="Location"
-                value={this.state.location}
-                onChange={this.handleChange}
-            />
-            <FieldGroup
-                label="Description"
-                name="description"
-                type="text"
-                placeholder="Description"
-                value={this.state.description}
-                onChange={this.handleChange}
-            />
-            <FieldGroup
-                label="Price"
-                name="price"
-                type="text"
-                placeholder="Ex: 100.50"
-                value={this.state.price}
-                onChange={this.handleChange}
-            />
-            <button
-              type="submit"
-              onClick={this.handleSubmit}
-              className="epSaveBtn"
-            >Submit</button>
-          </form>
+          <Row>
+            <Col xs={12} sm={12} md={6} lg={8}>
+              <form onSubmit={this.handleSubmit} className="sizeControl">
+                <FieldGroup
+                  label="Trip Name"
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                  value={this.state.name}
+                  onChange={this.handleChange}
+                />
+                <FieldGroup
+                    label="Trip location"
+                    name="location"
+                    type="text"
+                    placeholder="Location"
+                    value={this.state.location}
+                    onChange={this.handleChange}
+                />
+                <FieldGroup
+                    label="Price"
+                    name="price"
+                    type="text"
+                    placeholder="Ex: 100.50"
+                    value={this.state.price}
+                    onChange={this.handleChange}
+                />
+                <FormGroup controlId="formControlsTextarea">
+                  <ControlLabel>Description</ControlLabel>
+                  <FormControl
+                    componentClass="textarea"
+                    placeholder="Maximum of 250 words..."
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                    name="description"
+                    className="textArea"
+                    rows="10"
+                  />
+                </FormGroup>
+                <button
+                  type="submit"
+                  onClick={this.handleSubmit}
+                  className="epSaveBtn"
+                >Submit</button>
+              </form>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={4}>
+              <div className="container">
+                <button
+                  className="uploadWidget"
+                  onClick={this.uploadWidget}
+                  >Upload Photo</button>
+                <h4>Preview Trip Picture</h4>
+                  <img src={this.state.picture} className="profPic"/>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
     );
