43f1accaeb15de3a7ec0a9d8567855a044bf3044
Huck <evanallen51@gmail.com>
2018-06-30 16:56:41 -0500

    Redesigning companyDashboard menu and buttons

--- src/Containers/companyDashboard.jsx
+++ src/Containers/companyDashboard.jsx
@@ -19,6 +19,42 @@ class CompanyDashboard extends Component {
     return (
       <div className="container auto">
         <h1>Company Dashboard</h1>
+        <ul className="guideUl">
+          <Row className="container">
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <Link to={`/dashboard/${this.props.match.params.company}/trips`}>
+                <li className="btnCard">
+                  <h3 className="addNew">Manage Trips</h3>
+                  <h1 className="giantPlus">+</h1>
+                </li>
+              </Link>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <Link to={`/dashboard/${this.props.match.params.company}/guides`}>
+                <li className="btnCard">
+                  <h3 className="addNew">Manage Guides</h3>
+                  <h1 className="giantPlus">+</h1>
+                </li>
+              </Link>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <Link to={`dashboard/${this.props.match.params.company}/edit-company`}>
+                <li className="btnCard">
+                  <h3 className="addNew">Edit Company Details</h3>
+                  <h1 className="giantPlus">+</h1>
+                </li>
+              </Link>
+            </Col>
+            <Col xs={12} sm={6} md={4} lg={3}>
+              <Link to={`/dashboard/${this.props.match.params.company}/company-account`}>
+                <li className="btnCard">
+                  <h3 className="addNew">Company Account Info</h3>
+                  <h1 className="giantPlus">+</h1>
+                </li>
+              </Link>
+            </Col>
+          </Row>
+        </ul>
         <div className="container auto">
           <Row className="auto">
             <Col md={6}>
