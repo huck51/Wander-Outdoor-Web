f3bbf54f676dd9b15a85f982de743c071da06eb4
Huck <evanallen51@gmail.com>
2018-06-30 17:54:24 -0500

    Added some info to companyAccount

--- src/Containers/companyAccount.jsx
+++ src/Containers/companyAccount.jsx
@@ -6,14 +6,28 @@ class CompanyAccount extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      state: 'state',
+      companyCode: '',
     }
+  }
+
+  componentDidMount() {
+    axios.get(`https://fierce-ridge-55021.herokuapp.com/company/${this.props.match.params.company}`)
+      .then((response) => {
+        const { companyCode } = response.data
+        this.setState({
+          companyCode,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div>
-        <h1>Company Account Details</h1>
+        <h1>{`${this.props.match.params.company} Account Details`}</h1>
+        <p>Company Code: {this.state.companyCode}</p>
         <button>Delete Company</button>
       </div>
     );
