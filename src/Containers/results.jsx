290480b1a6fec3ca4f718c96c7df65ed8f5bc4cb
Huck <evanallen51@gmail.com>
2018-07-16 15:44:02 -0500

    Added componentDidMount function to results page

--- src/Containers/results.jsx
+++ src/Containers/results.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from '../Components/searchbar';
 import './Styles/results.css';
 
@@ -6,6 +7,22 @@ import './Styles/results.css';
 class Results extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      results: [],
+    };
+  }
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    axios.get(`https://fierce-ridge-55021.herokuapp.com/results/${params}`)
+      .then((result) => {
+        this.setState({
+          results: [...result.data],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
