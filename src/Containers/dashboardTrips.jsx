e57beee0bc204ec7e8ce63c33d38e33888665200
Huck <evanallen51@gmail.com>
2018-07-17 08:00:15 -0500

    Changed the link path to addTrip

--- src/Containers/dashboardTrips.jsx
+++ src/Containers/dashboardTrips.jsx
@@ -51,7 +51,7 @@ class DashboardTrips extends Component {
           <ul className="guideUl">
             <Row className="container">
               <Col xs={12} sm={6} md={4} lg={3}>
-                <Link to={`/dashboard/:company/add-trip`}>
+                <Link to={`/dashboard/${this.props.match.params.company}/add-trip`}>
                   <li className="btnCard">
                     <h3 className="addNew">Add New Trip</h3>
                     <h1 className="giantPlus">+</h1>
