5aa46692538481457fab9c31e7e9b75389282119
Huck <evanallen51@gmail.com>
2018-07-17 17:37:23 -0500

    Imported starRatingComponent to dashboardtrips

--- src/Containers/dashboardTrips.jsx
+++ src/Containers/dashboardTrips.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Thumbnail } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { BounceLoader } from 'react-spinners';
+import StarRatingComponent from 'react-star-rating-component';
 import axios from 'axios';
 import './Styles/dashboardGuides.css';
 
