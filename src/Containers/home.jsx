6bb3f45613fcb26b48cec008d23e191ef50bb0cf
Huck <evanallen51@gmail.com>
2018-07-16 23:03:02 -0500

    Made home a stateful component and updated it to properly interact with the new stateless searchbar component

--- src/Containers/home.jsx
+++ src/Containers/home.jsx
@@ -4,12 +4,27 @@ import './Styles/home.css';
 import '../App.css';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
   render() {
     return (
       <div>
         <div className="sbRug">
           <h2 id="topQ">Where will you wander?</h2>
-          <SearchBar id="LPSearch" />
+          <SearchBar
+            id="LPSearch"
+            val={this.state.value}
+            change={this.handleChange}
+          />
           <h2 id="bottomQ">What will you do?</h2>
         </div>
       </div>
