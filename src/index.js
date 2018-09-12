a0e7e89bbe033dca9ae56d4c070586cfb1ef4877
Huck <evanallen51@gmail.com>
2018-09-11 22:16:04 -0500

    Pushing

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import ReduxPromise from 'redux-promise';
 // import * as reducers from './Reducers/TBD'; // Change this when file created
 import './index.css';
 import App from './App';
-import MegaProvider from './Components/megaProvider';
 import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
@@ -17,9 +16,7 @@ const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 ReactDOM.render(
   (
     <BrowserRouter>
-      <MegaProvider>
         <App />
-      </MegaProvider>
     </BrowserRouter>
   ), document.getElementById('root'),
 );
