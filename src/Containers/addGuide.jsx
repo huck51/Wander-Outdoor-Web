9cba06bc5c0a84dbba0c4869c3f66c76ecbdb4bd
Huck <evanallen51@gmail.com>
2018-10-29 03:40:06 -0500

    Built out addGuide page

--- src/Containers/addGuide.jsx
+++ src/Containers/addGuide.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import axios from 'axios';
 import DisplayCard from '../Components/displayCard';
 import './Styles/addGuide.css';
@@ -9,18 +9,38 @@ class AddGuide extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      guides: []
+      guides: [],
+      addGuides: [],
     };
   }
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  select = (index) => {
+    console.log(index);
+    const tempGuides = this.state.guides;
+    const tempAddGuides = this.state.addGuides;
+    tempGuides[index].selected = !tempGuides[index].selected;
+    tempAddGuides[index].selected = !tempAddGuides[index].selected;
+    this.setState({
+      guides: tempGuides,
+      addGuides: tempAddGuides,
+    });
   }
   handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('https://fierce-ridge-55021.herokuapp.com/signup/guide')
+    const addGuides = this.state.addGuides;
+    const guides = [];
+    for (let i = 0; i <addGuides.length; i++) {
+      if (addGuides[i].selected) {
+        guides.push(addGuides[i]._id);
+      }
+    }
+    const payload = {
+      companyCode: this.props.match.params.company,
+      guides
+    }
+    axios.post('https://fierce-ridge-55021.herokuapp.com/add-guides-to-company', payload)
       .then(() => {
-        window.location = '/guides';
+        window.location = `/dashboard/${this.props.match.params.company}/guides`;
       })
       .catch((err) => {
         // eslint-disable-next-line no-console
@@ -32,8 +52,19 @@ class AddGuide extends Component {
     axios.get(`https://fierce-ridge-55021.herokuapp.com/guides/${this.props.match.params.company}`)
       .then((response) => {
         console.log(response);
+        const guides = [...response.data];
+        const addGuides = [];
+        for (let i = 0; i < guides.length; i++) {
+          guides[i].selected = false;
+          guides[i].index = i;
+          addGuides[i] = {
+            _id: guides[i]._id,
+            selected: false,
+          };
+        }
         this.setState({
-          guides: [...response.data],
+          guides,
+          addGuides,
         });
       })
       .catch((err) => {
@@ -41,14 +72,44 @@ class AddGuide extends Component {
       });
   }
   render() {
+    const selectText = {
+      color: 'rgb(40, 166, 76)',
+      textAlign: 'center',
+      margin: '2em auto',
+    };
     return (
       <div>
         <h1>Add Guide</h1>
+        <button onClick={this.handleSubmit}>Add Selected Guides</button>
         <ul className="guideUl">
           <Row className="container">
             {this.state.guides.map((guide) => {
               return (
-                <DisplayCard item={guide} Url={`/profile/${guide.id}`} />
+                <Col xs={12} sm={6} md={4} lg={3}>
+                  <li className={this.state.guides[guide.index].selected ? `list cursor selectedCard` : `list cursor`} onClick={() => this.select(guide.index)}>
+                    <div className="thumbox" style={{marginBottom: 0}}>
+                      <div
+                        style={
+                          {
+                            backgroundImage: `url(${guide.picture})`,
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: '100% auto',
+                            backgroundColor: 'white',
+                          }
+                        }
+                        className="cardImg"
+                      />
+                      <div className="caption">
+                        <h3>{guide.name}</h3>
+                        <p>{`${guide.city}, ${guide.stateName}`}</p>
+                      </div>
+                      <p style={selectText}>
+                        <strong>{this.state.guides[guide.index].selected ? 'SELECTED' : ''}</strong>
+                      </p>
+                    </div>
+                  </li>
+                </Col>
               );
             })}
           </Row>
