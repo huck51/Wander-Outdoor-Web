9f5e7575a5783196efc2ca4bfdf3d1ae30858dd7
Huck <evanallen51@gmail.com>
2018-10-30 11:50:09 -0500

    Built out reviewCard Component

---
+++ src/Components/reviewCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import StarRatingComponent from 'react-star-rating-component';
+
+const ReviewCard = ({review, index}) => {
+  return (
+    <Col xs={12} sm={6} md={4} lg={3}>
+      <li>
+        <div>
+          <StarRatingComponent
+            name={review.author + index}
+            starColor="#3783B6"
+            emptyStarColor="#B5D994"
+            value={review.rate}
+            className="starRating"
+            editing={false}
+          />
+          <p>{review.text}</p>
+          <p>-{review.author}</p>
+          <p>{review.date}</p>
+        </div>
+      </li>
+    </Col>
+  );
+};
+
+export default ReviewCard;
