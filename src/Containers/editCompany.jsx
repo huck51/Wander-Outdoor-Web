e26103505f7bf7cda225f6bfb1226b2b5b72c215
Huck <evanallen51@gmail.com>
2018-11-05 22:38:16 -0600

    Changed sizing of pictures on edit pages

--- src/Containers/editCompany.jsx
+++ src/Containers/editCompany.jsx
@@ -37,7 +37,6 @@ class EditCompany extends Component {
 
   componentDidMount() {
     const id = localStorage.getItem('fierceIce');
-    // CHANGE URL BEFORE SERIOUS TESTING !!!!
     axios.get(`https://fierce-ridge-55021.herokuapp.com/company/${this.props.match.params.company}`)
     .then((response) => {
       console.log(response.data);
