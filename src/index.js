4600de687552819935a7bb3760063aa3051890e5
Huck <evanallen51@gmail.com>
2018-11-07 16:59:27 -0600

    Got rid of redux code in index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { StripeProvider } from 'react-stripe-elements';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
-// import * as reducers from './Reducers/TBD'; // Change this when file created
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   (
@@ -24,15 +18,3 @@ ReactDOM.render(
   ), document.getElementById('root'),
 );
 registerServiceWorker();
-/*
-ReactDOM.render(
-  (
-    <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  ), document.getElementById('root'),
-);
-registerServiceWorker();
-*/
