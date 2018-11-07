b57bee3b0b540798738f14aeff4cb8e4acc19000
Huck <evanallen51@gmail.com>
2018-11-07 16:51:36 -0600

    Removed unused redux packages and files to improve performance

--- src/Reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import * as reducers from './TBD'; // Change this to file that I create to hold combineReducers
-
-const rootReducer = combineReducers(reducers);
-
-export default rootReducer;
