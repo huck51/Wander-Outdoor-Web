11ff7bc79c1b25cbca169f577240d23433f789ef
Huck <evanallen51@gmail.com>
2019-03-09 21:07:10 -0500

    Implemented searchbar on Trips page

--- src/Containers/trips.jsx
+++ src/Containers/trips.jsx
@@ -3,6 +3,7 @@ import { Row, Col } from 'react-bootstrap';
 import { BounceLoader } from 'react-spinners';
 import axios from 'axios';
 import ProfileList from '../Components/profileList';
+import SearchBar from '../Components/searchbar';
 import './Styles/trips.css';
 
 
@@ -16,6 +17,10 @@ class Trips extends Component {
   }
 
   componentDidMount() {
+    this.getAllTrips();
+  }
+
+  getAllTrips = () => {
     axios.get('https://fierce-ridge-55021.herokuapp.com/trips')
       .then((result) => {
         // eslint-disable-next-line no-console
@@ -34,10 +39,42 @@ class Trips extends Component {
       });
   }
 
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const search = this.state.value;
+    if (search.trim() === '') {
+      this.getAllTrips();
+    } else {
+      axios.get(`https://fierce-ridge-55021.herokuapp.com/search/Trip/${search}`)
+        .then((result) => {
+          console.log(result);
+          this.setState({
+            trips: result.data[0],
+            loading: false
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          this.setState({ loading: false });
+        });
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>Trips</h1>
+        <h1>TRIPS</h1>
+        <form onSubmit={this.handleSubmit} >
+          <SearchBar
+            val={this.state.value}
+            change={this.handleChange}
+            submit={this.handleSubmit}
+          />
+        </form>
         <div className="boxOfCards">
           <Row>
             <Col md={2} mdOffset={5}>
