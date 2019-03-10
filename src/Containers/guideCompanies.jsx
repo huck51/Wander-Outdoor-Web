aa7c8e4912e175c047e7295ee85f26763c15e611
Huck <evanallen51@gmail.com>
2019-03-09 20:57:38 -0500

    Implemented searchbar on Guiding Companies page'

--- src/Containers/guideCompanies.jsx
+++ src/Containers/guideCompanies.jsx
@@ -3,6 +3,7 @@ import { Row, Col } from 'react-bootstrap';
 import { BounceLoader } from 'react-spinners';
 import axios from 'axios';
 import ProfileList from '../Components/profileList';
+import SearchBar from '../Components/searchbar';
 import './Styles/guideCompanies.css';
 
 
@@ -16,6 +17,10 @@ class GuideCompanies extends Component {
   }
 
   componentDidMount() {
+    this.getAllCompanies();
+  }
+
+  getAllCompanies = () => {
     axios.get('https://fierce-ridge-55021.herokuapp.com/guiding-companies')
       .then((result) => {
         this.setState({
@@ -32,10 +37,42 @@ class GuideCompanies extends Component {
       });
   }
 
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const search = this.state.value;
+    if (search.trim() === '') {
+      this.getAllCompanies();
+    } else {
+      axios.get(`https://fierce-ridge-55021.herokuapp.com/search/Company/${search}`)
+        .then((result) => {
+          console.log(result);
+          this.setState({
+            companies: result.data[0],
+            loading: false
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          this.setState({ loading: false });
+        });
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>GUIDING COMPANIES</h1>
+        <form onSubmit={this.handleSubmit} >
+          <SearchBar
+            val={this.state.value}
+            change={this.handleChange}
+            submit={this.handleSubmit}
+          />
+        </form>
         <div className="boxOfCards">
           <Row>
             <Col md={2} mdOffset={5}>
